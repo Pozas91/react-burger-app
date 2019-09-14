e3d712447afea493eb0036e5e882fc333eadd469
Pozas91 <pozas91@gmail.com>
2019-09-14 13:54:58 +0200

    Using updateObject in the Entire App

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -12,6 +12,8 @@ import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 
 import * as actions from '../../../store/actions/index';
 
+import {updateObject} from "../../../shared/utility";
+
 
 class ContactData extends Component {
     state = {
@@ -143,18 +145,16 @@ class ContactData extends Component {
     };
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        };
 
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        };
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        });
 
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        });
 
         let formIsValid = true;
 
@@ -162,7 +162,6 @@ class ContactData extends Component {
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
         }
 
-        // console.log(updatedFormElement);
         console.log(formIsValid);
 
         this.setState({
